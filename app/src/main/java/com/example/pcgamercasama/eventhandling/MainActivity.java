94ba0c6df89ace512f74606bc6a3507bfbe25f64
bhaya001 <mouadbhaya@gmail.com>
2016-10-06 00:16:51 +0100

    First Commit

---
+++ app/src/main/java/com/example/pcgamercasama/eventhandling/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pcgamercasama.eventhandling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        TextView text=(TextView)findViewById(R.id.text);
+                        text.setText("Application Testee avec succees");
+                    }
+                }
+        );
+    }
+}
